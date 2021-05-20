f0029dd3dc34675f2eabada878f384811acf76b9
Frederico Santana <fredericosantana@hotmail.com.br>
2021-05-20 00:53:05 -0300

    primeiro upload

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+
+//usando template engine
+server.set('view engine', 'ejs')
+
+//habilitar arquivos estáticos
+server.use(express.static("public"))
+
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
